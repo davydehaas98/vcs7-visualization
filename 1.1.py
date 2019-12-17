d8d9d748f379d374f5a4f62588c8038a00a82575
Davy de Haas <davy.dehaas98@gmail.com>
2019-12-17 20:10:37 +0100

    Add own camera to 1.1

--- 1.1.py
+++ 1.1.py
@@ -1,5 +1,6 @@
 from vtkmodules.all import \
-    vtkRenderWindow, vtkRenderer, vtkRenderWindowInteractor, vtkInteractorStyleTrackballCamera,\
+    vtkRenderWindow, vtkRenderer, vtkCamera, \
+    vtkRenderWindowInteractor, vtkInteractorStyleTrackballCamera, \
     vtkConeSource, vtkPolyDataMapper, vtkActor
 
 
@@ -8,19 +9,24 @@ class WindowRenderer:
     def __init__(self):
         self.renderer = vtkRenderer()
 
+        self.__camera = vtkCamera()
         self.__render_window = vtkRenderWindow()
         self.__render_window_interactor = vtkRenderWindowInteractor()
         self.__interactor_style_trackball_camera = vtkInteractorStyleTrackballCamera()
 
     def setup_render_window(self):
         """Setup the render window"""
-        # Set renderer background to grey
-        self.renderer.SetBackground(1.0, 0.0, 0.5)
 
-        # Add renderer to render window
-        self.__render_window.AddRenderer(self.renderer)
+        # Set camera
+        self.__camera.SetPosition(0.0, 0.0, 20.0)
+        self.__camera.SetFocalPoint(0.0, 0.0, 0.0)
+
+        # Set renderer
+        self.renderer.SetActiveCamera(self.__camera)
+        self.renderer.SetBackground(0.0, 0.0, 1.0)
 
         # Set render window
+        self.__render_window.AddRenderer(self.renderer)
         self.__render_window.SetSize(1000, 600)
 
         # Set render window interactor
@@ -29,6 +35,7 @@ class WindowRenderer:
 
     def start_render_window(self):
         """Start the render window"""
+
         # Initialize interactor
         self.__render_window_interactor.Initialize()
 
@@ -47,29 +54,31 @@ class Cone:
         self.__cone_mapper = vtkPolyDataMapper()
         self.__cone_actor = vtkActor()
 
-    def setup_cone(self):
+    def setup_cone(self, position):
         """Setup the cone"""
+
         # Set cone properties
         self.__cone.SetRadius(1.0)
         self.__cone.SetHeight(2.0)
         self.__cone.SetResolution(50)
+        self.__cone.SetCenter(position)
 
         # Set cone mapper
         self.__cone_mapper.SetInputConnection(self.__cone.GetOutputPort())
 
         # Set cone actor
         self.__cone_actor.SetMapper(self.__cone_mapper)
-        self.__cone_actor.SetPosition(0.0, 0.0, 0.5)
         self.__cone_actor.GetProperty().SetColor(1.0, 0.0, 0.0)
 
         # Add cone actor to the window renderer
         self.__renderer.AddActor(self.__cone_actor)
 
 
+# Run the program
 if __name__ == '__main__':
     window_renderer = WindowRenderer()
-    cone = Cone(window_renderer.renderer)
+    Cone(window_renderer.renderer).setup_cone((2.0, 2.0, 2.0))
+    Cone(window_renderer.renderer).setup_cone((0.0, 0.0, 0.0))
 
-    cone.setup_cone()
     window_renderer.setup_render_window()
     window_renderer.start_render_window()
