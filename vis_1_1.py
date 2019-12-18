fbbe028d43e09e1f2309669e0c20d8549a9c0e22
Davy de Haas <davy.dehaas98@gmail.com>
2019-12-18 13:44:35 +0100

    Add vis_1_2

--- vis_1_1.py
+++ vis_1_1.py
@@ -1,47 +1,8 @@
-from vtkmodules.all import \
-    vtkRenderWindow, vtkRenderer, vtkCamera, \
-    vtkRenderWindowInteractor, vtkInteractorStyleTrackballCamera, \
-    vtkConeSource, vtkPolyDataMapper, vtkActor
+from vtkmodules.all import (
+    vtkConeSource, vtkPolyDataMapper, vtkActor,
+)
 
-
-class WindowRenderer:
-
-    def __init__(self):
-        self.renderer = vtkRenderer()
-
-        self.__camera = vtkCamera()
-        self.__render_window = vtkRenderWindow()
-        self.__render_window_interactor = vtkRenderWindowInteractor()
-        self.__interactor_style_trackball_camera = vtkInteractorStyleTrackballCamera()
-
-    def setup_render_window(self):
-        """Setup the render window"""
-
-        # Set camera
-        self.__camera.SetPosition(0.0, 0.0, 20.0)
-        self.__camera.SetFocalPoint(0.0, 0.0, 0.0)
-
-        # Set renderer
-        self.renderer.SetActiveCamera(self.__camera)
-        self.renderer.SetBackground(0.0, 0.0, 1.0)
-
-        # Set render window
-        self.__render_window.AddRenderer(self.renderer)
-        self.__render_window.SetSize(1000, 600)
-
-        # Set render window interactor
-        self.__render_window_interactor.SetRenderWindow(self.__render_window)
-        self.__render_window_interactor.SetInteractorStyle(self.__interactor_style_trackball_camera)
-
-    def start_render_window(self):
-        """Start the render window"""
-
-        # Initialize interactor
-        self.__render_window_interactor.Initialize()
-
-        # Start render window with interactor
-        self.__render_window.Render()
-        self.__render_window_interactor.Start()
+from library.window_renderer import WindowRenderer
 
 
 class Cone:
--- vis_1_2.py
+++ vis_1_2.py
@@ -1,4 +1,9 @@
-from vtkmodules.all import vtkTextSource, vtkPolyDataMapper, vtkActor
+from vtkmodules.all import (
+    vtkTextSource, vtkPolyDataMapper, vtkActor,
+)
+
+from library.window_renderer import WindowRenderer
+
 
 class Text:
     def __init__(self, renderer):
@@ -9,11 +14,12 @@ class Text:
         self.__text_mapper = vtkPolyDataMapper()
         self.__text_actor = vtkActor()
 
-    def setup_text(self):
+    def setup_text(self, text):
         """Setup text"""
 
         # Set text
-        self.__text.SetText("Hello World")
+        self.__text.SetText(text)
+        self.__text.SetForegroundColor(1.0, 0.0, 0.0)
 
         # Set text mapper
         self.__text_mapper.SetInputConnection(self.__text.GetOutputPort())
@@ -21,10 +27,12 @@ class Text:
         # set text actor
         self.__text_actor.SetMapper(self.__text_mapper)
 
+        self.__renderer.AddActor(self.__text_actor)
+
 
 # Run the program
 if __name__ == '__main__':
-    # window_renderer = WindowRenderer()
-    #
-    # window_renderer.setup_render_window()
-    # window_renderer.start_render_window()
+    window_renderer = WindowRenderer()
+    Text(window_renderer.renderer).setup_text("Hello World")
+    window_renderer.setup_render_window()
+    window_renderer.start_render_window()
