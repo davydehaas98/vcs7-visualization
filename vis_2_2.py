e7b9f63ad634432399667c1f1322f69e3a83ca59
Davy de Haas <davy.dehaas98@gmail.com>
2020-01-05 16:24:52 +0100

    Add optional arguments to vis_2_2

--- vis_2_2.py
+++ vis_2_2.py
@@ -1,5 +1,6 @@
 from vtkmodules.all import (
-    vtkPolyDataReader,vtkPolyDataMapper,
+    vtkPolyDataReader, vtkTransform,
+    vtkTransformFilter, vtkPolyDataMapper,
     vtkProperty, vtkActor,
     vtkStructuredPointsReader, vtkGeometryFilter,
 )
@@ -14,24 +15,42 @@ class VTKPolyDataVisualizer:
         self.__renderer = renderer
 
         self.__reader = vtkPolyDataReader()
-        self.__geometry_filter = vtkGeometryFilter()
+        self.__transform = vtkTransform()
+        self.__transform_filter = vtkTransformFilter()
         self.__mapper = vtkPolyDataMapper()
         self.__property = vtkProperty()
         self.__actor = vtkActor()
 
-    def setup(self, file_name, color):
+    def setup(self, file_name, color=None, opacity=None, position=None, rotation=None):
         """Setup the VTK poly data visualizer"""
+
+        # Set optional arguments
+        color = color or (1.0, 0.0, 0.0)
+        opacity = opacity or 1.0
+        rotation = rotation or (0.0, 0.0, 0.0, 0.0)
+        position = position or (0.0, 0.0, 0.0)
 
         # Set reader
         self.__reader.SetFileName(file_name)
         self.__reader.Update()
 
+
+        # Set transform
+        self.__transform.RotateWXYZ(*rotation)
+        self.__transform.Translate(position)
+
+        # Set transform filter
+        self.__transform_filter.SetInputConnection(self.__reader.GetOutputPort())
+        self.__transform_filter.SetTransform(self.__transform)
+        self.__transform_filter.Update()
+
         # Set mapper
-        self.__mapper.SetInputConnection(self.__reader.GetOutputPort())
+        self.__mapper.SetInputConnection(self.__transform_filter.GetOutputPort())
         self.__mapper.Update()
 
         # Set property
         self.__property.SetColor(color)
+        self.__property.SetOpacity(opacity)
 
         # Set actor
         self.__actor.SetMapper(self.__mapper)
@@ -53,8 +72,11 @@ class VTKStructuredPointsVisualizer:
         self.__property = vtkProperty()
         self.__actor = vtkActor()
 
-    def setup(self, file_name):
+    def setup(self, file_name, color=None):
         """Setup the VTK structured points visualizer"""
+
+        # Set optional arguments
+        color = color or (1.0, 0.0, 0.0)
 
         # Set reader
         self.__reader.SetFileName(file_name)
@@ -70,7 +92,7 @@ class VTKStructuredPointsVisualizer:
         self.__mapper.Update()
 
         # Set property
-        self.__property.SetColor(1.0, 0.0, 0.0)
+        self.__property.SetColor(color)
 
         # Set  actor
         self.__actor.SetMapper(self.__mapper)
@@ -84,7 +106,7 @@ class VTKStructuredPointsVisualizer:
 if __name__ == '__main__':
     __window_renderer = WindowRenderer()
     # VtkPolyDataReader cannot read dataset type: structured_points
-    # VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/brain.vtk")
+    #VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/brain.vtk")
     VTKStructuredPointsVisualizer(__window_renderer.renderer).setup("objects/brain.vtk")
 
     __window_renderer.setup_render_window((0.0, 0.0, 1000.0))
--- vis_2_6.py
+++ vis_2_6.py
@@ -1,27 +1,34 @@
+from vtkmodules.all import (
+    vtkNamedColors,
+)
 from utils.window_renderer import WindowRenderer
 from vis_2_2 import VTKPolyDataVisualizer
 
-
 if __name__ == '__main__':
     __window_renderer = WindowRenderer()
+    __colors = vtkNamedColors()
 
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/blood.vtk", (1.0, 0.0, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/brain.vtk", (0.5, 0.0, 0.5))
-    #VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/brainbin.vtk", (0.0, 0.0, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/duodenum.vtk", (1.0, 0.25, 0.25))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/eye_retina.vtk", (0.0, 1.0, 1.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/eye_white.vtk", (1.0, 1.0, 1.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/heart.vtk", (1.0, 0.0, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/ileum.vtk", (1.0, 0.25, 0.25))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/kidney.vtk", (0.5, 0.25, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/large_intestine.vtk", (0.5, 0.25, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/liver.vtk", (0.5, 0.25, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/lung.vtk", (1.0, 0.75, 0.75))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/nerves.vtk", (0.0, 1.0, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/skeleton.vtk", (0.75, 0.75, 0.75))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/skin.vtk", (0.0, 0.5, 0.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/spleen.vtk", (0.0, 0.0, 1.0))
-    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/stomach.vtk", (0.0, 0.0, 1.0))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/blood.vtk", color=__colors.GetColor3d("salmon"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/brain.vtk", color=__colors.GetColor3d("beige"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/duodenum.vtk", color=__colors.GetColor3d("orange"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/eye_retina.vtk", color=__colors.GetColor3d("misty_rose"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/eye_white.vtk", color=__colors.GetColor3d("white"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/heart.vtk", color=__colors.GetColor3d("tomato"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/ileum.vtk", color=__colors.GetColor3d("raspberry"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/kidney.vtk", color=__colors.GetColor3d("banana"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/large_intestine.vtk", color=__colors.GetColor3d("peru"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/liver.vtk", color=__colors.GetColor3d("pink"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/lungs.vtk", color=__colors.GetColor3d("powder_blue"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/nerves.vtk", color=__colors.GetColor3d("carrot"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/skeleton.vtk", color=__colors.GetColor3d("wheat"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup(
+        "objects/frog/skin.vtk",
+        color=__colors.GetColor3d("green"),
+        opacity=0.4,
+        rotation=(180.0, 0.0, 0.0, 1.0)
+    )
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/spleen.vtk", color=__colors.GetColor3d("violet"))
+    VTKPolyDataVisualizer(__window_renderer.renderer).setup("objects/frog/stomach.vtk", color=__colors.GetColor3d("plum"))
 
     __window_renderer.setup_render_window((0.0, 0.0, 750.0))
     __window_renderer.start_render_window()
