7ba4735e353f5fbbe90b53ec316922118abc71d9
Davy de Haas <davy.dehaas98@gmail.com>
2020-01-16 15:47:44 +0100

    Refactor window.py

--- utils/window.py
+++ utils/window.py
@@ -6,63 +6,67 @@ from vtkmodules.all import (
 )
 
 
+def create_camera(position) -> vtkCamera:
+    """Setup camera"""
+
+    # Set camera
+    camera = vtkCamera()
+    camera.SetClippingRange(1.0, 2000.0)
+    camera.SetFocalPoint(0.0, 0.0, 0.0)
+    camera.SetPosition(position)
+    camera.SetViewUp(1.0, 1.0, 1.0)
+    camera.Zoom(1.0)
+
+    # Divides the camera's distance from the focal point by the given dolly value
+    camera.Dolly(1.0)
+    camera.ComputeViewPlaneNormal()
+
+    return camera
+
+
+def create_light(position) -> vtkLight:
+    """Setup light"""
+
+    # Set light
+    light = vtkLight()
+    light.SetColor(1.0, 1.0, 1.0)
+    light.SetFocalPoint(0.0, 0.0, 0.0)
+    light.SetPosition(position)
+    light.PositionalOn()
+
+    return light
+
+
 class Window:
 
     def __init__(self):
         self.renderer = vtkRenderer()
 
-        self.__camera = vtkCamera()
-        self.__light = vtkLight()
-        self.__render_window = vtkRenderWindow()
-        self.__render_window_interactor = vtkRenderWindowInteractor()
-        self.__interactor_style_trackball_camera = vtkInteractorStyleTrackballCamera()
-
     def setup(self, position):
         """Setup the render window"""
 
         # Set camera
-        self.__setup_camera(position)
+        camera = create_camera(position)
 
         # Set light
-        self.__setup_light(position)
+        light = create_light(position)
 
         # Set renderer
-        self.renderer.SetActiveCamera(self.__camera)
-        #self.renderer.AddLight(self.__light)
-        self.renderer.SetBackground(0.5, 1.0, 1.0)
+        self.renderer.SetActiveCamera(camera)
+        #self.renderer.AddLight(light)
+        self.renderer.SetBackground(0.9, 0.9, 0.9)
 
         # Set render window
-        self.__render_window.AddRenderer(self.renderer)
-        self.__render_window.SetSize(1000, 600)
+        render_window = vtkRenderWindow()
+        render_window.AddRenderer(self.renderer)
+        render_window.SetSize(1000, 600)
 
         # Set render window interactor
-        self.__render_window_interactor.SetRenderWindow(self.__render_window)
-        self.__render_window_interactor.SetInteractorStyle(self.__interactor_style_trackball_camera)
-        self.__render_window_interactor.Initialize()
+        render_window_interactor = vtkRenderWindowInteractor()
+        render_window_interactor.SetRenderWindow(render_window)
+        render_window_interactor.SetInteractorStyle(vtkInteractorStyleTrackballCamera())
+        render_window_interactor.Initialize()
 
         # Render the window and start the interactor
-        self.__render_window.Render()
-        self.__render_window_interactor.Start()
-
-    def __setup_camera(self, position):
-        """Setup camera"""
-
-        # Set camera
-        self.__camera.SetClippingRange(1.0, 2000.0)
-        self.__camera.SetFocalPoint(0.0, 0.0, 0.0)
-        self.__camera.SetPosition(position)
-        self.__camera.SetViewUp(1.0, 1.0, 1.0)
-        self.__camera.Zoom(1.0)
-
-        # Divides the camera's distance from the focal point by the given dolly value
-        self.__camera.Dolly(1.0)
-        self.__camera.ComputeViewPlaneNormal()
-
-    def __setup_light(self, position):
-        """Setup light"""
-
-        # Set light
-        self.__light.SetColor(1.0, 1.0, 1.0)
-        self.__light.SetFocalPoint(0.0, 0.0, 0.0)
-        self.__light.SetPosition(position)
-        self.__light.PositionalOn()
+        render_window.Render()
+        render_window_interactor.Start()
